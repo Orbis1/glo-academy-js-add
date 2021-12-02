c410f56ce0cbf5e60bc6100848b8f8ff4dace8ef
Orbis <infectores@gmail.com>
2021-12-02 14:52:57 +0300

    recursion and closure

--- script.js
+++ script.js
@@ -9,50 +9,58 @@ const getRandomIntInclusive = function(min, max) {
    (max - min + 1)) + min; //Максимум и минимум включаются
 };
 
-const secret = getRandomIntInclusive(START, END);
-console.log(secret);
+const game = function(secret) {
+  const num = secret;
 
-const dialog = function(num) {
+  const dialog = function() {
 
-  const answer = prompt(`Guess a number from ${START} to ${END}?`);
+    const answer = prompt(`Guess a number from ${START} to ${END}?`);
 
-  const responseTo = function(ans) {
-    switch (true) {
-    case ans === null:
-      return { msg: 'Game Over', gameover: true };
-    case isNaN(+ans):
-      return { msg: 'Enter the number, please', gameover: false };
-    case +ans > END || +ans < START:
-      return {
-        msg: `The number must be in the range from ${START} to ${END}`,
-        gameover: false
-      };
-    case +ans > num:
-      return {
-        msg: `The secret number is less then ${ans}`,
-        gameover: false
-      };
-    case +ans < num:
-      return {
-        msg: `The secret number is greater then ${ans}`,
-        gameover: false
-      };
-    case +ans === num:
-      return {
-        msg: `You guessed! It is ${ans}`,
-        gameover: true
-      };
-    default:
-      return {
-        msg: `You entered ${ans}. Please try again!`,
-        gameover: true
-      };
-    }
+    const responseTo = function(ans) {
+      switch (true) {
+      case ans === null:
+        return { msg: 'Game Over', gameover: true };
+      case isNaN(+ans):
+        return { msg: 'Enter the number, please', gameover: false };
+      case +ans > END || +ans < START:
+        return {
+          msg: `The number must be in the range from ${START} to ${END}`,
+          gameover: false
+        };
+      case +ans > num:
+        return {
+          msg: `The secret number is less then ${ans}`,
+          gameover: false
+        };
+      case +ans < num:
+        return {
+          msg: `The secret number is greater then ${ans}`,
+          gameover: false
+        };
+      case +ans === num:
+        return {
+          msg: `You guessed! It is ${ans}`,
+          gameover: true
+        };
+      default:
+        return {
+          msg: `You entered ${ans}. Please try again!`,
+          gameover: false
+        };
+      }
+    };
+
+    return responseTo(answer);
   };
 
-  const { msg, gameover } = responseTo(answer);
-  alert(msg);
-  if (!gameover) dialog(num);
+  return dialog;
 };
 
-dialog(secret);
+const round1 = game(getRandomIntInclusive(START, END));
+
+const startGame = function() {
+  const { msg, gameover } = round1();
+  alert(msg);
+  if (!gameover) startGame();
+};
+startGame();
