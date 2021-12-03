487fd1e7249f46e2b45148b87c9e901f47732e51
Orbis <infectores@gmail.com>
2021-12-03 21:07:19 +0300

    major

--- script.js
+++ script.js
@@ -1,81 +1,44 @@
 'use strict';
 
-const START = 1, END = 100;
-const TRIES_COUNT = 5;
+// 1) Создать массив week и записать в него дни недели в виде строк
+const week = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
-const getRandomIntInclusive = function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() *
-   (max - min + 1)) + min; //Максимум и минимум включаются
-};
+// Вывести на экран все дни недели
+// Каждый из них с новой строчки
+// Выходные дни - курсивом
+// Текущий день - жирным шрифтом(использовать объект даты)
 
-const guesser = function(ans, num, count) {
+function getTodayName() {
+  return new Date().toLocaleString('en-GB', { weekday: 'long' });
+}
 
-  if (count === 0) return {
-    msg: `${count} tries left. Do you want play again?`,
-    gameover: true,
+const days = week.map(day => {
+  const obj = {
+    dayName: day,
+    isToday: day === getTodayName(),
   };
+  return obj;
+});
 
-  switch (true) {
-  case ans === null:
-    return { msg: 'Game Over', gameover: true };
-  case isNaN(+ans):
-    return { msg: 'Enter the number, please', gameover: false };
-  case +ans > END || +ans < START:
-    return {
-      msg: `The number must be in the range from ${START} to ${END}`,
-      gameover: false,
-    };
-  case +ans > num:
-    return {
-      msg: `The secret number is less then ${ans}. ${count} tries left`,
-      gameover: false,
-    };
-  case +ans < num:
-    return {
-      msg: `The secret number is greater then ${ans}. ${count} tries left`,
-      gameover: false,
-    };
-  case +ans === num:
-    return {
-      msg: `You guessed! It is ${ans}. Do you want play again?`,
-      gameover: true,
-    };
-  default:
-    return {
-      msg: `You entered ${ans}. Please try again!`,
-      gameover: false,
-    };
-  }
+const renderList = () => {
+  const list = days.map(({ isToday, dayName }) => {
+    const listItem = `
+      <li ${isToday ? 'style = "font-weight: bold;"' : ''}>
+        ${dayName}
+      </li>`;
+    return listItem;
+  });
+  return `<ul>${list.join('')}</ul>`;
 };
 
-const game = secret => count => rules => {
+document.body.innerHTML = `<div>${renderList()}</div>`;
 
-  const answer = prompt(`Guess a number from ${START} to ${END}?`);
-  const { msg, gameover } = rules(answer, secret, count);
-  let repeat = false;
-
-  if (gameover) {
-    if (msg === 'Game Over') {
-      repeat = false;
-    } else {
-      repeat = confirm(msg);
-    }
-  } else {
-    alert(msg);
-    repeat = game(secret)(--count)(rules);
-  }
-
-  return repeat;
-
-};
-
-const letsPlay = round => {
-  document.title = `Guesser. Round ${round}`;
-  const secret = getRandomIntInclusive(START, END);
-  const playAgain = game(secret)(TRIES_COUNT)(guesser);
-  if (playAgain) letsPlay(++round);
-};
-
-letsPlay(1);
+// 2) Запушить проект в репозиторий для усложненных заданий на Github
